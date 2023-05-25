e415d9cc6061532f271c8993ae368721600d8bd3
Brian-ONeil <oneilbrianlee@gmail.com>
2023-05-25 16:45:46 -0500

    updating files

--- evaluate_oneil.py
+++ evaluate_oneil.py
@@ -18,6 +18,8 @@ from sklearn.preprocessing import MinMaxScaler
 
 import lugo_wrangle as wl
 import wrangle as wra
+import corey_model as cm
+
 
 ### cluster, stats functions
 
@@ -88,19 +90,116 @@ def density_chlorides_cluster_ttest(X_tr2, y_tr):
 
     
     
-X_tr_sc = pd.concat([X_tr_sc, X_tr2['cluster']], axis=1)
-X_val_sc = pd.concat([X_val_sc, X_tr2['cluster']], axis=1) 
+def get_metrics_with_cluster(X_tr2, X_tr_sc, y_tr, X_val_sc, y_val, alpha=1, power=2, degrees=2):
+    """
+    This function will
+    - accept X_tr_sc, y_tr, X_val_sc, y_val
+    - accept values for alpha, power, and degrees; default values are 1/2/2
+        - alpha is a hyperparameter for LassoLARS
+        - power is a hyperparameter for Polynomial Regressioin
+        - degrees is a hyperparameter for GLM
+    - add cluster column to X_tr_sc and X_val_sc
+    - create dummy variables for cluster column with drop_first=True
+    - drop cluster column from X_tr_sc and X_val_sc
+    - drop rows with missing values from X_val_sc
+    - call get_reg_model_metrics_df function and return the resulting dataframe
+    """
+    # add cluster column to X_tr_sc and X_val_sc
+    X_tr_sc = pd.concat([X_tr_sc, X_tr2['cluster']], axis=1)
+    X_val_sc = pd.concat([X_val_sc, X_tr2['cluster']], axis=1)
     
-dummies = pd.get_dummies(X_tr_sc['cluster'], prefix='cluster', drop_first=True)
-X_tr_sc = pd.concat([X_tr_sc, dummies], axis=1)    
-dummies = pd.get_dummies(X_val_sc['cluster'], prefix='cluster', drop_first=True)
-X_val_sc = pd.concat([X_val_sc, dummies], axis=1)
+    # create dummy variables for cluster column with drop_first=True
+    dummies = pd.get_dummies(X_tr_sc['cluster'], prefix='cluster', drop_first=True)
+    X_tr_sc = pd.concat([X_tr_sc, dummies], axis=1)
+    dummies = pd.get_dummies(X_val_sc['cluster'], prefix='cluster', drop_first=True)
+    X_val_sc = pd.concat([X_val_sc, dummies], axis=1)
+    
+    # drop cluster column from X_tr_sc and X_val_sc
+    X_tr_sc = X_tr_sc.drop('cluster', axis=1)
+    X_val_sc = X_val_sc.drop('cluster', axis=1)
+    
+    # drop rows with missing values from X_val_sc
+    X_val_sc = X_val_sc.dropna()
+    
+    # call get_reg_model_metrics_df function and return the resulting dataframe
+    return cm.get_reg_model_metrics_df(X_tr_sc, y_tr, X_val_sc, y_val, alpha=alpha, power=power, degrees=degrees)
 
-X_tr_sc = X_tr_sc.drop('cluster', axis=1)
+def plot_cluster_2(X_tr, y_tr):
+    tr = X_tr.join(y_tr)
+    tr['quality_cat'] = pd.cut(tr['quality'], bins=[2, 6, 9], labels=['3-6', '7-9'])
 
-X_val_sc = X_val_sc.drop('cluster', axis=1)
+    y_tr2 = tr[['quality_cat']]
 
-X_val_sc = X_val_sc.dropna()
+    X_tr3 = X_tr[['alcohol', 'residual_sugar']]
 
-cm.get_reg_model_metrics_df(X_tr_sc, y_tr, X_val_sc, y_val
-                            ,alpha=1, power=2, degrees=2)
+    #make it
+    kmeans = KMeans(n_clusters = 3, random_state=123, n_init=10)
+
+    #fit it
+    kmeans.fit(X_tr3)
+
+    #use it
+    kmeans.predict(X_tr3)
+
+    # And assign the cluster number to a column on the dataframe
+    X_tr3["cluster"] = kmeans.predict(X_tr3)
+
+    # create the first scatter plot
+    sns.relplot(data=X_tr3,
+               x='alcohol',
+               y='residual_sugar',
+               hue='cluster',
+               col='cluster',
+               col_wrap=3)
+
+    # create the second scatter plot
+    sns.relplot(data=X_tr3.join(y_tr2),
+               x='alcohol',
+               y='residual_sugar',
+               hue='quality_cat',
+               col='cluster',
+               col_wrap=3)
+
+    # show the plots
+    plt.show()
+    
+
+def plot_cluster_3(X_tr, y_tr):
+    tr = X_tr.join(y_tr)
+    tr['quality_cat'] = pd.cut(tr['quality'], bins=[2, 6, 9], labels=['3-6', '7-9'])
+
+    y_tr2 = tr[['quality_cat']]
+    
+    X_tr4 = X_tr[['total_sulfur_dioxide', 'residual_sugar']]
+    
+    #make it
+    kmeans = KMeans(n_clusters = 3, random_state=123, n_init=10)
+
+    #fit it
+    kmeans.fit(X_tr4)
+
+    #use it
+    kmeans.predict(X_tr4)
+    
+    # And assign the cluster number to a column on the dataframe
+    X_tr4["cluster"] = kmeans.predict(X_tr4)
+    
+    # create the first scatter plot
+    sns.relplot(data=X_tr4,
+               x='total_sulfur_dioxide',
+               y='residual_sugar',
+               hue='cluster',
+               col='cluster',
+               col_wrap=3)
+
+    # create the second scatter plot
+    sns.relplot(data=X_tr4.join(y_tr2),
+               x='total_sulfur_dioxide',
+               y='residual_sugar',
+               hue='quality_cat',
+               col='cluster',
+               col_wrap=3)
+
+    # show the plots
+    plt.show()
+    
